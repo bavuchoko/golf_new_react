75a9dce54986ffd85ee91f221674ccaebb20e306
bavuchoko <bavuchoko@naver.com>
2024-04-05 23:08:44 +0900

    경기장 목록 조회 api

--- src/api/field/FieldService.js
+++ src/api/field/FieldService.js
@@ -5,8 +5,29 @@ async function createField(field) {
     return await needAuth.post('/field', field);
 }
 
-async function getFieldList(search) {
-    return  await noAuh.get('/field', {params: {searchTxt: search}});
+async function getFieldList(search, pageable) {
+    const start = performance.now();
+    let pageStr="";
+    let sort="sort=";
+    let size='&size=';
+    let page='&page=';
+    const desc = ',desc';
+    if(pageable.sort) {
+        sort += pageable.sort
+        if (pageable.desc) {
+            sort += desc;
+        }
+    }
+
+    if(pageable.size){
+        size +=pageable.size;
+    }
+    page +=pageable.page;
+    pageStr=sort+size+page;
+    return await needAuth.get('/field?' + pageStr, search);
+
 }
+
+
 
 export {createField, getFieldList};
