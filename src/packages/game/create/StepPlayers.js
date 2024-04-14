41b8ba0ad058223494c46fd8027492b356e6562b
DESKTOP-0POT47S\bavuc <bavuchoko@nvaer.com>
2024-04-14 13:21:55 +0900

    경기생성 기본 화면

--- src/packages/game/create/StepPlayers.js
+++ src/packages/game/create/StepPlayers.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import ToggleSwitch from "../../../components/toggle/ToggleSwitch";
 
 function StepPlayers({setStep, data, fnc}) {
+
+    const [playerCnt, setPlayerCnt] =useState(3);
     const navigate = useNavigate();
     return (
-        <div className={"px-[30px]"}>
-            <div className="w-full line-h-40 py-[5px] line-h-50 h-[55px] ">
+        <div className={"px-[10px]"}>
+            <div className="px-[20px] w-full line-h-40 py-[5px] line-h-50 h-[55px] ">
                 <div className="inline-block w-[100%] flex h-[50px]" >
                     <p onClick={()=>navigate(-1)}>뒤로</p>
                     <div className={"ml-auto w-[36px]"}>
@@ -13,8 +16,21 @@ function StepPlayers({setStep, data, fnc}) {
                             setStep('경기장')
                         }}>다음</span>
                     </div>
-
                 </div>
+            </div>
+
+
+            <div className={'mt-[3rem] player-name-container w-full' }>
+                <div className={`each-player`}>
+                    <input type={`text`} className={``} readOnly={true} value={'박종수'}/>
+                </div>
+                {Array.from({length: playerCnt}).map((_, index) =>(
+                    <div className={`each-player`}>
+                        <input type={`text`} className={``} placeholder={ `${index+2} 번`}/>
+                    </div>
+                ))}
+
+
             </div>
         </div>
     );
