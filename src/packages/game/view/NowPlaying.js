473b352e4510ca408f0c153e57a4f2be46696aff
pjs <bavuchoko@naver.com>
2024-06-14 11:29:04 +0900

    선수 선택, 점수변경을 sheet 받아서 처리하도록

--- src/packages/game/view/NowPlaying.js
+++ src/packages/game/view/NowPlaying.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import NumberSelector from "../../../components/selectbox/NumberSelector";
 import {useSelector} from "react-redux";
 import ViewScoreList from "./components/ViewScoreList";
-
+import {Counter, LefterBtn, PlayerDiv, RighterBtn} from "./style/StyleView";
 
 
 function NowPlaying({data}) {
@@ -10,6 +10,36 @@ function NowPlaying({data}) {
     const user = useSelector((state) => state.user.user);
     const isHost = user.id===data.host.id
     const [showCurrentRound, setShowCurrentRound] = useState(true);
+    const initialClickedPlayer = () => {
+        if (!data.sheets || data.sheets.length === 0) {
+            return {};
+        }
+
+        // 가장 큰 round 값 찾기
+        const maxRound = Math.max(...data.sheets.map(sheet => sheet.round));
+        const maxRoundSheets = data.sheets.filter(sheet => sheet.round === maxRound);
+
+        // round-1의 요소 찾기
+        const roundMinusOne = maxRound - 1;
+        const roundMinusOneSheets = data.sheets.filter(sheet => sheet.round === roundMinusOne);
+
+        // 조건에 맞는 요소 찾기
+        let selectedSheet;
+        if (roundMinusOneSheets.length > 0) {
+            // 최저 hit 찾기
+            const minHit = Math.min(...roundMinusOneSheets.map(sheet => sheet.hit));
+            // 최저 hit를 가진 요소들 찾기
+            const minHitSheets = roundMinusOneSheets.filter(sheet => sheet.hit === minHit);
+            // id가 가장 작은 요소 선택
+            selectedSheet = minHitSheets.reduce((prev, current) => (prev.player.id < current.player.id ? prev : current));
+        } else {
+            selectedSheet = maxRoundSheets.reduce((prev, current) => (prev.player.id < current.player.id ? prev : current));
+        }
+
+        return selectedSheet;
+    };
+
+    const [clickedPlayer, setClickedPlayer] = useState(initialClickedPlayer);
 
     const playerHits = {};
     data.sheets.forEach(record => {
@@ -20,8 +50,10 @@ function NowPlaying({data}) {
             playerHits[playerId].totalHits += hit;
         } else {
             playerHits[playerId] = {
+                id:playerId,
                 name: playerName,
-                totalHits: hit
+                totalHits: hit,
+                sheet :record
             };
         }
     });
@@ -38,10 +70,9 @@ function NowPlaying({data}) {
 
 
 
-
-    const playerClickHandler =()=>{
+    const playerClickHandler =(sheet)=>{
         if(isHost){
-
+            setClickedPlayer(sheet)
         }
     }
 
--- src/packages/game/view/components/ViewScoreList.js
+++ src/packages/game/view/components/ViewScoreList.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import styled, {keyframes} from "styled-components";
+import {CurrentRound, ScoreList, ScoreListContainer, TotalScore} from "../style/StyleView";
 
 function ViewScoreList({data, isHost, showCurrentRound, setShowCurrentRound}) {
 
@@ -8,99 +8,53 @@ function ViewScoreList({data, isHost, showCurrentRound, setShowCurrentRound}) {
         vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
     }, []);
-    const ScoreList = styled.div`
-        text-align: center;
-        border: 1px solid #d5d5d5;
-        margin-bottom: 5px;
-        height: ${props => props.isHost ? 'calc(var(--vh, 1vh) * 100 - 280px)' : 'calc(var(--vh, 1vh) * 100 - 125px)'};
-    `;
 
-    const ScoreListContainer = styled.div`
-        position: relative;
-        text-align: center;
-        border: 1px solid #d5d5d5;
-        margin-bottom: 5px;
-        overflow-y: auto;
-        height: ${props => props.isHost ? 'calc(var(--vh, 1vh) * 100 - 330px)' : 'calc(var(--vh, 1vh) * 100 - 175px)'};
-    `;
-    const CurrentRound = styled.button`
-        text-align: center;
-        position: absolute;
-        top:0;
-        left: ${props => props.visable ? '0' : '-100%'};
-        animation: ${props => props.visable ? slideLeftIn : slideLeftOut} 0.5s cubic-bezier(0.68, -0.45, 0.27, 1.25) forwards;;
-        border: 1px solid #d5d5d5;
-        margin-bottom: 5px;
-        width: 100%;
-        background: white;
-        overflow-y: auto;
-  
-        height: ${props => props.isHost ? 'calc(var(--vh, 1vh) * 100 - 330px)' : 'calc(var(--vh, 1vh) * 100 - 175px)'};
-    `;
-    const TotalScore = styled.button`
-        text-align: center;
-        position: absolute;
-        top:0;
-        left: ${props => props.visable ? '100%' : '0'};
-        animation: ${props => props.visable ? slideRightOut : slideRightIn} 0.5s cubic-bezier(0.68, -0.45, 0.27, 1.25) forwards;;
-        transition: left 0.5s ease;
-        border: 1px solid #d5d5d5;
-        margin-bottom: 5px;
-        width: 100%;
-        background: white;
-        overflow-y: auto;
+    const sheets = data.sheets;
 
-        height: ${props => props.isHost ? 'calc(var(--vh, 1vh) * 100 - 330px)' : 'calc(var(--vh, 1vh) * 100 - 175px)'};
-    `;
-    const slideLeftIn = keyframes`
-        from {
-            left: -100%;
+    // round 별로 시트를 그룹화
+    const groupedSheets = sheets.reduce((acc, sheet) => {
+        const round = sheet.round;
+        if (!acc[round]) {
+            acc[round] = [];
         }
-        to {
-            left: 0;
-        }
-    `;
-
-    const slideLeftOut = keyframes`
-        from {
-            left: 0;
-        }
-        to {
-            left: -100%;
-        }
-    `;
-    const slideRightIn = keyframes`
-        from {
-            left: 100%;
-        }
-        to {
-            left: 0;
-        }
-    `;
-
-    const slideRightOut = keyframes`
-        from {
-            left: 0;
-        }
-        to {
-            left: 100%;
-        }
-    `;
-
-
+        acc[round].push(sheet);
+        return acc;
+    }, {});
+    const minRound = Math.min(...Object.keys(groupedSheets));
+    console.log(groupedSheets)
     return (
         <ScoreList isHost={isHost}>
-            <div className={`flex h-[50px] line-h-50 justify-center w-full`}>
+            <div className={`flex h-[50px] line-h-50 justify-center w-full border`}>
                 <div className={`w-[120px] text-left ${showCurrentRound ? 'font-bold' : ''}`}  onClick={()=>setShowCurrentRound(true)}>현재 라운드</div>
                 <div className={`splicer h-[30px] mt-[10px]`}/>
                 <div className={`w-[120px] text-right ${!showCurrentRound ? 'font-bold' : '' }`} onClick={()=>setShowCurrentRound(false)}>라운드 합계</div>
             </div>
             <ScoreListContainer isHost={isHost}>
                 <CurrentRound isHost={isHost} visable={showCurrentRound}>
-                    {data.id}
+
+                    <div className={`grid grid-cols-5 mb-2`}>
+                            <div>홀</div>
+                            {groupedSheets[minRound].map(round =>(
+                            <div>
+                                {round.player.name}
+                            </div>
+                            ))}
+                        </div>
+                    {Object.keys(groupedSheets).map((round,index) => (
+                        <div key={round} className={`grid grid-cols-5 py-1 ${index % 2 === 0 ? 'bg-odd' :'bg-even'}`}>
+                            <div>{round}</div>
+                            {groupedSheets[round].sort((a, b) => {
+                                const playerOrder = groupedSheets[minRound].map(sheet => sheet.player.id);
+                                return playerOrder.indexOf(a.player.id) - playerOrder.indexOf(b.player.id);
+                            }).map(sheet => (
+                                <div key={sheet.id}>
+                                    <p className={``}>{sheet.hit}</p>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </CurrentRound>
-                <TotalScore isHost={isHost} visable={showCurrentRound}>
-                    {data.host.name}
+                <TotalScore data={data} isHost={isHost} visable={showCurrentRound}>
                 </TotalScore>
             </ScoreListContainer>
         </ScoreList>
