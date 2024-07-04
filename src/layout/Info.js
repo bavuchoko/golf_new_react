e6d4ff09d517e20738408f951310cb5d26c7651c
bavuchoko <bavuchoko@naver.com>
2024-07-04 22:14:07 +0900

    메인화면 버튼 위치 fix

--- src/layout/Info.js
+++ src/layout/Info.js
@@ -9,13 +9,15 @@ function Info(props) {
     return (
         <>
             <BeforLoginHeader />
-            <div className="containers">
+            <div className="relative">
                 <div className={`section-init-container`}>
 
                 </div>
                 <div className={`section-init-btn-container`}>
-                    <Link to={"/game/quick"} className={`mr-[20px]`} >빠른시작</Link>
-                    <Link to={"/game/create"}>연습경기</Link>
+                    <div className={`w-[320px] m-auto`}>
+                        <Link to={"/game/quick"} className={`mr-[20px]`} >빠른시작</Link>
+                        <Link to={"/game/create"}>연습경기</Link>
+                    </div>
                 </div>
             </div>
         </>
