6f3e45a2f6ed8ecd8e1aed527199b0dd66d46d34
pjs <bavuchoko@naver.com>
2024-07-07 13:31:14 +0900

    메인화면 기본 디자인

--- src/layout/container/style/StyleView.js
+++ src/layout/container/style/StyleView.js
@@ -5,7 +5,8 @@ import shouldForwardProp from "@styled-system/should-forward-prop";
 export const IntroContainer = styled.div.withConfig({
     shouldForwardProp: (prop) => shouldForwardProp(prop) && prop !== 'isLoggedIn'
 })`
-    
+    background: #fafafa;
+    height: ${props => props.isLoggedIn ? 'calc(var(--vh, 1vh) * 100 - 45px)' : 'calc(var(--vh, 1vh) * 100 - 65px)'}; 
 `;
 export const IntroSection = styled.div.withConfig({
     shouldForwardProp: (prop) => shouldForwardProp(prop) && prop !== 'isLoggedIn'
@@ -37,8 +38,9 @@ export const IntroBlueRoundButton = styled.a`
     font-size: 15px;
     border-radius: 30px;
     background-color: var(--main-btn-color);
-    padding: 8px;
+    padding: 8px 6px;
     color: white;
-    width: 150px;
+    width: 130px;
     text-align: center;
+    box-shadow:0 4px 9px 2px hsla(210, 2%, 50%, 0.4);
 `;
