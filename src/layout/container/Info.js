cd4846966c46ec2b6b8008499a76f156836f0e4b
pjs <bavuchoko@naver.com>
2024-07-20 19:02:17 +0900

    불필요한 임포트 제거

--- src/layout/container/Info.js
+++ src/layout/container/Info.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {Link, Outlet} from "react-router-dom";
 import BeforLoginHeader from "../header/BeforLoginHeader";
-import {useSelector} from "react-redux";
-import MainHeader from "../header/MainHeader";
 import IntroPage from "./IntroPage";
 
 function Info(props) {
