0e20c7d3803469fa6ff428f700cfabf264d95496
pjs <bavuchoko@naver.com>
2024-08-30 20:28:03 +0900

    근처 경기장 조회결과 없을때 메세지 수정

--- src/components/exception/Nocontent.js
+++ src/components/exception/Nocontent.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Nocontent(props) {
+function Nocontent({message}) {
     return (
-        <div className={"pt-[40%] w-full text-center text-[16px]"}>조회 결과가 없습니다.</div>
+        <div className={"pt-[40%] w-full text-center text-[16px]"}>{message ? message : "조회 결과가 없습니다."}</div>
     );
 }
 
